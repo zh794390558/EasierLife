a3316f0c2f91a23c5fa274d5af71e3f24b03823f
zhanghui <zhanghui@unisound.com>
2017-11-21 02:49:13 +0000

    support chinese QA

--- Plugins/Tuling/tuling.py
+++ Plugins/Tuling/tuling.py
@@ -10,39 +10,52 @@ except:
 
 def get_response(msg, storageClass = None, userName = None, userid = 'ItChat'):
     url = 'http://www.tuling123.com/openapi/api'
+    if isinstance(msg, str):
+        msg = msg.decode('utf-8')
+        print(type(msg))
+
     payloads = {
         'key': key,
         'info': msg,
         'userid': userid,
     }
     try:
-        r = requests.post(url, data = json.dumps(payloads)).json()
+        #r = requests.post(url, data = json.dumps(payloads)).json()
+        r = requests.post(url, data = payloads).json()
     except:
         return
+
+    print(type(r))
+
     if not r['code'] in (100000, 200000, 302000, 308000, 313000, 314000): return
-    if r['code'] == 100000: # 文本类
-        return '\n'.join([r['text'].replace('<br>','\n')])
-    elif r['code'] == 200000: # 链接类
+    if r['code'] == 100000: # tuling.py
+
+        #return '\n'.join([r['text'].replace('<br>','\n')])
+        return r.get('text').encode('utf-8')
+
+    elif r['code'] == 200000: # tuling.py
         return '\n'.join([r['text'].replace('<br>','\n'), r['url']])
-    elif r['code'] == 302000: # 新闻类
+    elif r['code'] == 302000: # tuling.py
         l = [r['text'].replace('<br>','\n')]
         for n in r['list']: l.append('%s - %s'%(n['article'], n['detailurl']))
         return '\n'.join(l)
-    elif r['code'] == 308000: # 菜谱类
+    elif r['code'] == 308000: # tuling.py
         l = [r['text'].replace('<br>','\n')]
         for n in r['list']: l.append('%s - %s'%(n['name'], n['detailurl']))
         return '\n'.join(l)
-    elif r['code'] == 313000: # 儿歌类
+    elif r['code'] == 313000: # tuling.py
         return '\n'.join([r['text'].replace('<br>','\n')])
-    elif r['code'] == 314000: # 诗词类
+    elif r['code'] == 314000: # tuling.py
         return '\n'.join([r['text'].replace('<br>','\n')])
 
 if __name__ == '__main__':
     try:
         ipt = raw_input
-        ipt = lambda: raw_input('>').decode(sys.stdin.encoding)
+        #ipt = lambda: raw_input('>').decode(sys.stdin.encoding)
     except:
         ipt = lambda: input('>')
     while True:
         a = ipt()
+        print(a)
+        print(type(a))
         print(get_response(a, 'ItChat'))
